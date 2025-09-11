4efa94eaf39765bed2809418b9fd78950dd88c48
radu <chetreanalexandru63@gmail.com>
2025-09-11 22:07:48 +0200

    OOP

--- OOP.cpp
+++ OOP.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,17 +14,33 @@ private:
     uint speed;
     float height;
     float weight;
-    int strength;
+    uint strength;
+
+protected:
+    int chance()
+    {
+        uint roll = rand() % 100 + 1;
+        if (roll <= 80)
+            return strength;
+        else if (roll <= 90)
+            return strength + (strength * 50 / 100);
+        else
+        {
+            allStatsBoost();
+            return strength;
+        }
+    }
+
+    void allStatsBoost()
+    {
+        health += health / 10;
+        strength += strength / 10;
+        speed += speed / 10;
+    }
 
 public:
-    Obj(uint hea, string n, uint sp, float hei, float w, int str)
+    Obj(uint hea, string n, uint sp, float hei, float w, uint str)
         : health(hea), name(n), speed(sp), height(hei), weight(w), strength(str) {}
-
-    virtual void attack() = 0;
-    virtual void walking() = 0;
-    virtual void recovery() = 0;
-    virtual void strong() = 0;
-    virtual string getName() = 0;
 
     virtual ~Obj() {}
 };
