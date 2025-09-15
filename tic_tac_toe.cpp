0cf9a1ff06b75bdcd4a732a5d32eabaca20a217a
radu <chetreanalexandru63@gmail.com>
2025-09-15 22:17:45 +0200

    tictactoe

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -3,7 +3,6 @@
 #include <string>
 
 using namespace std;
-
 using Board = array<char, 9>;
 
 void printBoard(const Board &board)
@@ -11,9 +10,11 @@ void printBoard(const Board &board)
     cout << "\n";
     for (int i = 0; i < 9; i += 3)
     {
-        cout << " " << (b[i] == ' ' ? to_string(i + 1) : string(1, b[i]))
-             << " | " << (b[i + 1] == ' ' ? to_string(i + 2) : string(1, b[i + 1]))
-             << " | " << (b[i + 2] == ' ' ? to_string(i + 3) : string(1, b[i + 2])) << "\n";
+        cout << " " << (board[i] == ' ' ? to_string(i + 1) : string(1, board[i]))
+             << " | " << (board[i + 1] == ' ' ? to_string(i + 2) : string(1, board[i + 1]))
+             << " | " << (board[i + 2] == ' ' ? to_string(i + 3) : string(1, board[i + 2]))
+             << "\n";
+
         if (i < 6)
             cout << "---+---+---\n";
     }
@@ -27,46 +28,44 @@ char checkResult(const Board &board)
 
     for (auto &w : wins)
     {
-        char a = b[w[0]], c = b[w[1]], d = b[w[2]];
-        if (a != ' ' && a == c && a == d)
+        char a = board[w[0]];
+        char b = board[w[1]];
+        char c = board[w[2]];
+        if (a != ' ' && a == b && a == c)
             return a;
     }
 
-    bool anyEmpty = false;
-    for (char cell : b)
+    for (char cell : board)
+    {
         if (cell == ' ')
-        {
-            anyEmpty = true;
-            break;
-        }
-    if (!anyEmpty)
-        return 'D';
+            return ' ';
+    }
 
-    return ' ';
+    return 'D';
 }
 
-int getPlayerMove(const Board &board, char player)
+int getPlayerMove(Board &board, char player)
 {
     while (true)
     {
         cout << "Player " << player << ", enter cell number (1-9): ";
-        string s;
-        if (!getline(cin, s))
-            return -1;
-        if (s.empty())
+        string input;
+        getline(cin, input);
+
+        if (input.empty())
             continue;
 
         try
         {
-            int pos = stoi(s);
+            int pos = stoi(input);
             if (pos < 1 || pos > 9)
             {
-                cout << "Invalid number. Please enter a number from 1 to 9.\n";
+                cout << "Invalid number. Enter a number from 1 to 9.\n";
                 continue;
             }
-            if (b[pos - 1] != ' ')
+            if (board[pos - 1] != ' ')
             {
-                cout << "The cell is occupied. Choose another.\n";
+                cout << "That cell is already taken. Choose another one.\n";
                 continue;
             }
             return pos - 1;
@@ -81,6 +80,7 @@ int getPlayerMove(const Board &board, char player)
 int main()
 {
     cout << "=== Tic Tac Toe (3x3) ===\n";
+
     while (true)
     {
         Board board;
@@ -88,38 +88,29 @@ int main()
 
         char current = 'X';
         char result = ' ';
+
         printBoard(board);
 
         while (result == ' ')
         {
             int idx = getPlayerMove(board, current);
-            if (idx == -1)
-            {
-                cout << "\nInput complete. Game stopped.\n";
-                return 0;
-            }
             board[idx] = current;
             printBoard(board);
+
             result = checkResult(board);
+
             if (result == ' ')
-            {
                 current = (current == 'X') ? 'O' : 'X';
-            }
         }
 
         if (result == 'D')
-        {
-            cout << "Tie!\n";
-        }
+            cout << "It's a draw!\n";
         else
-        {
-            cout << "Player  won " << result << "!\n";
-        }
+            cout << "Player " << result << " wins!\n";
 
         cout << "Play again? (y/n): ";
         string again;
-        if (!getline(cin, again))
-            break;
+        getline(cin, again);
         if (again.empty() || (again[0] != 'y' && again[0] != 'Y'))
             break;
     }
