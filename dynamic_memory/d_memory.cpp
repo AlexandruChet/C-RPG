d3bb490ae9af0354fa448dd4815af525efff2077
radu <chetreanalexandru63@gmail.com>
2025-12-22 20:49:22 +0100

    update dynamic memory fire

--- dynamic_memory/d_memory.cpp
+++ dynamic_memory/d_memory.cpp
@@ -6,19 +6,36 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "n = ?" << " ";
+    cout << "n = ? ";
     cin >> n;
 
-    string *strings = new string[n];
-    getline(cin, strings[0]);
+    if (n <= 0)
+    {
+        cout << "The number of lines should be > 0\n";
+        return 1;
+    }
 
-    for (int i = 0; i < n; i++) 
+    cin.ignore();
+
+    string* strings = new string[n];
+
+    for (int i = 0; i < n; i++)
         getline(cin, strings[i]);
 
-    for (int i; i < n; i++)
-        cout << strings[i] << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << ": " << strings[i] << endl;
+        cout << "Total number of characters: "
+             << strings[i].length() << endl;
+    }
+
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++)
+        if (strings[i].length() > strings[maxIndex].length())
+            maxIndex = i;
+
+    cout << "The longest line is: " << strings[maxIndex] << endl;
 
     delete[] strings;
-
     return 0;
 }
